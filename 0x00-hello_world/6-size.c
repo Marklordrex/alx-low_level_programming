e8335b7f143b0b36e3fea6864f20ba78aa42f6f0
Marklordrex <Marklordrex@gmail.com>
2023-06-17 13:08:19 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,8 +4,6 @@
  *
  * Return: Always (0);
  */
-
-
 int main(void)
 {
 	printf("size of char: %d byte(s)\n", sizeof(char));
@@ -14,6 +12,4 @@ int main(void)
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-
-
 }
