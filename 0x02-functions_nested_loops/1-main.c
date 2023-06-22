8f34dab068a9002cab45c5b8a61220e5586182f7
Marklordrex <Marklordrex@gmail.com>
2023-06-22 10:27:51 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * main - check the code
  * Return: Always 0.
@@ -7,6 +8,9 @@
 
 int main(void)
 {
-            print_alphabet();
-            return (0);
+
+	print_alphabet();
+	return (0);
 }
+
+
