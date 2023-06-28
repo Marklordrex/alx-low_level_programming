9e9ad2273ce122f0440502e59cc09c5abc753c3d
Marklordrex <Marklordrex@gmail.com>
2023-06-28 09:59:23 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,7 @@
 
 
 /**
-* main - prints the numbers from 1 to 10, followed by a new line
+* main - prints the numbers from 1 to 100, followed by a new line
 * but for multiples of three print Fizz instead of the number
 * and for the multiples of five prints Buzz
 * Return: Always 0 (Success)
