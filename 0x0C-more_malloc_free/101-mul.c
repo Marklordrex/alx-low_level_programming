1e8a523353d734c73abc7a1c4b81671c92d50987
Marklordrex <Marklordrex@gmail.com>
2023-07-14 08:13:55 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -123,7 +123,7 @@ for (l1 = 0; argv[1][l1]; l1++)
 for (l2 = 0; argv[2][l2]; l2++)
 ;
 ln = l1 + l2 + 1;
-a = malloc(ln * sizeof(char));
+a = malloc(sizeof(char) * ln);
 if (a == NULL)
 {
 for (ti = 0; e[ti]; ti++)
@@ -144,4 +144,4 @@ exit(98);
 }
 _print(a, ln - 1);
 return (0);
-i}
+}
